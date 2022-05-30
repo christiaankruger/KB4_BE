4d99cbbd347307014c49b0c38490841cbfadf20b
Christiaan Kruger <c@hristiaan.co.za>
2022-05-30 18:21:57 +0200

    Implement graphql query, limit and offset against types with required fields

--- lib/spacex_api/spacex_api.rb
+++ lib/spacex_api/spacex_api.rb
@@ -1,40 +1,83 @@
-require "graphql/client"
-require "graphql/client/http"
-
-def with_find(fields)
-  { fields: fields, has_find: true }
-end
-
-def without_find(fields)
-  { fields: fields, has_find: false }
-end
+require 'graphql/client'
+require 'graphql/client/http'
 
 module SpaceXAPI
-  SEARCHABLE_ENTITIES = {
-    dragons: ([:name, :description]),
-    histories: ([:details]),
-    landpads: ([:full_name, :details]),
-    launches: ([:details, :mission_name]),
-    launchpads: ([:name, :details]),
-    missions: ([:description, :name]),
-    rockets: ([:name, :description]),
-    ships: ([:name]),
-  }
-  HTTP = GraphQL::Client::HTTP.new("https://api.spacex.land/graphql")
+  HTTP = GraphQL::Client::HTTP.new('https://api.spacex.land/graphql')
   Schema = GraphQL::Client.load_schema(HTTP)
   Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
 
   DragonsQuery = Client.parse <<-GRAPHQL
+  query($limit: Int, $offset: Int) {
+    dragons(limit: $limit, offset: $offset) {
+      id
+      name
+      description
+    }
+  }
+  GRAPHQL
+
+  HistoriesQuery = Client.parse <<~GRAPHQL
     query($limit: Int, $offset: Int) {
-      dragons(limit: $limit, offset: $offset) {
+      histories(limit: $limit, offset: $offset) {
+        id
+        details
+      }
+    }
+  GRAPHQL
+  LandpadsQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      landpads(limit: $limit, offset: $offset) {
+        id
+        full_name
+        details
+      }
+    }
+  GRAPHQL
+  LaunchesQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      launches(limit: $limit, offset: $offset) {
+        id
+        details
+        mission_name
+      }
+    }
+  GRAPHQL
+  LaunchpadsQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      launchpads(limit: $limit, offset: $offset) {
         id
         name
+        details
+      }
+    }
+  GRAPHQL
+  MissionsQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      missions(limit: $limit, offset: $offset) {
+        id
         description
+        name
+      }
+    }
+  GRAPHQL
+  RocketsQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      rockets(limit: $limit, offset: $offset) {
+        id
+        description
+        name
+      }
+    }
+  GRAPHQL
+  ShipsQuery = Client.parse <<~GRAPHQL
+    query($limit: Int, $offset: Int) {
+      ships(limit: $limit, offset: $offset) {
+        id
+        name
       }
     }
   GRAPHQL
 
-  def self.search
-
-  end
+  QUERIES = [DragonsQuery, HistoriesQuery, LandpadsQuery, LaunchesQuery, LaunchpadsQuery, MissionsQuery, RocketsQuery,
+             ShipsQuery].freeze
 end
--- lib/spacexsearch.rb
+++ lib/spacexsearch.rb
@@ -1,12 +1,54 @@
 require_relative './spacex_api/spacex_api'
+require_relative './find/find'
 
 module SpaceXSearch
+  BATCH_SIZE = 200
+
   def self.search(options)
     search_term = options[:search]
 
-    data = SpaceXAPI::Client.query(SpaceXAPI::DragonsQuery, variables: {limit: 0, offset: 0})
-    p data
+    puts "Searching for '#{search_term}'..."
+
+    # Run every type of query on its own thread
+    threads = SpaceXAPI::QUERIES.map do |query|
+      Thread.new do
+        data_hashes = []
+        limit = BATCH_SIZE
+        offset = 0
+
+        loop do
+          data = SpaceXAPI::Client.query(query, variables: { limit: limit, offset: offset }).original_hash['data']
+          finder = Find.new(data)
+
+          break if finder.items.empty?
+
+          data_hashes.push(data)
+          break if finder.items.length < BATCH_SIZE
+
+          offset += BATCH_SIZE
+        end
+
+        all_results = data_hashes.map do |data|
+          Find.new(data).find_term(search_term)
+        end
+
+        type = all_results.first[:type]
+        ids = all_results.map { |result| result[:ids] }.flatten
+        Thread.current[:output] = { type: type, ids: ids }
+      end
+    end
+
+    all_results = threads.each_with_object([]) do |t, result|
+      t.join
+      result.push(t[:output])
+
+      result
+    end.each_with_object([]) do |result, memo|
+      memo.push(result) unless result[:ids].empty?
+    end
+
+    puts "I have found '#{search_term}' in the following object types:"
+    puts ''
+    puts all_results
   end
 end
-
-# [:description, :name, :details]
